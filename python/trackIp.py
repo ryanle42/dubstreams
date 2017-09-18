c8039a449a4194d8c0ae17aba899dfc0413361b4
ryan <ryancode42@gmail.com>
2017-09-17 20:27:17 -0700

    lots of changes..

--- python/trackIp.py
+++
@@ -1,8 +0,0 @@
-import sys
-from time import localtime, strftime
-
-if (sys.argv[2].find("css") < 0 and sys.argv[2].find("mkv") < 0 and sys.argv[2].find("mp4") < 0):
-	f=open("./ipAddresses.txt", "a+")
-	time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-	f.write("ip: (" + sys.argv[1] + ") path: (" + sys.argv[2] + ") time: (" + time + ")\n\n")
-	f.close()
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var spawn = require("child_process").spawn;
+var device = require('express-device');
+var http = require('http');
+var lineReader = require('line-reader');
+var fs = require('fs');
 
 app.use(session({
 	secret: 'super secret', 
@@ -12,8 +16,45 @@ app.use(session({
 	saveUninitialized: true
 }));
 
+
+
+app.use(device.capture());
+
+
+// Track request and ban ip that makes a db request
+
+
+// Check Blacklist
+app.use(function (req, res, next) {
+	var banned = false;
+    var request_ip = req.ip 
+            || req.connection.remoteAddress 
+            || req.socket.remoteAddress 
+            || req.connection.socket.remoteAddress;
+    if (request_ip.substr(0, 7) == "::ffff:") {
+	  request_ip = request_ip.substr(7)
+	}
+	if (fs.existsSync('blacklist.txt')) {
+		lineReader.eachLine('blacklist.txt', function(ip, last_ip) {
+			if (ip == request_ip) {
+				banned = true;
+				res.status(403).end('forbidden');
+			} 
+			if (last_ip && (banned == false)) {
+				next();
+			}
+		});
+	} else {
+		next();
+	}
+});
+
 app.use(function(req, res, next) {
-	spawn('python',["./python/trackIp.py", req.ip, req.path]);
+	var request_ip = req.ip;
+	if (request_ip.substr(0, 7) == "::ffff:") {
+	  request_ip = request_ip.substr(7)
+	}
+	spawn('python',["./python/trackIp.py", request_ip, req.path]);
 	next();
 });
 
@@ -34,16 +75,29 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	if (req.device.type != 'desktop') {
+		res.render('error/device-not-supported');
+	}
+	else {
+		res.render('index');
+	}
 });
 
 // GoT Season 7
 app.get('/gameofthrones/*', function(req,res) {
-	res.render(req.path.slice(1));
+	if (req.device.type != 'desktop') {
+		res.render('error/device-not-supported');
+	} else {
+		res.render(req.path.slice(1));
+	}
 });
 
 app.get('/rickandmorty/*', function(req,res) {
-	res.render(req.path.slice(1));
+	if (req.device.type != 'desktop') {
+		res.render('error/device-not-supported');
+	} else {
+		res.render(req.path.slice(1));
+	}
 });
 
 
